b755531eac27115b84c4b20a4b8bf2a896c5f2ee
Ogbonna Chibuoyim <ogbonnachibuoyim12@gmail.com>
2019-10-28 18:28:47 +0100

    template.cpp
    My template for competitive programming tasks.

---
+++ template.cpp
@@ -0,0 +1,27 @@
+/*
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma comment(linker, "/stack:200000000")
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) v.begin(),v.end()
+#define watch(x) cerr<<"->"<<#x<<": "<<x<<endl;
+// #define int long long
+typedef pair<int,int> pii;
+const int INF = 1000000000;
+const int MOD = 1000000007;
+const long long INFLL = 1e18;
+const double PI = 2*acos(0.0);
+signed main()
+{
+	//freopen("","r",stdin);
+	//freopen("","w",stdout);
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	clock_t tickx = clock();
+	// code starts here
+	clock_t ticky = clock();
+	fprintf(stderr, "Runtime: %.4f seconds", double(ticky - tickx) / CLOCKS_PER_SEC);
+	return 0;	
+}
