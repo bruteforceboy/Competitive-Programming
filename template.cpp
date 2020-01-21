5596c5f4ad6225099984f5d67a1a8dfe42a93bec
Ogbonna Chibuoyim <ogbonnachibuoyim12@gmail.com>
2020-01-21 02:25:38 +0100

    update template.cpp
    My C++ template for programming contests.

--- template.cpp
+++ template.cpp
@@ -2,24 +2,32 @@
 using namespace std;
 
 #define endl '\n'
-
-#define PI 2 * acos(0.0);
-#define INFLL 1e18
-#define CLEAR(x, val) memset(x, val, sizeof(x))
-#define print(x) cout << x << endl, exit(0)
-#define watch(x) cerr << #x << " -> " << x << endl
 // #define int long long
+
+#define watch(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1) {
+   cerr << name << ": " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args) {
+   const char* comma = strchr(names + 1, ',');
+   cerr.write(names, comma - names) << ": " << arg1 << " |";
+  __f(comma + 1, args...);
+}
 
 typedef long long ll;
 typedef long double ld;
+
+typedef vector<int> vi;
 typedef pair<int, int> pii;
 
 const int INF = 1000000000;
-const int MOD = 1000000007; //998244353
+const int MOD = 1000000007; // 998244353
 
-signed main() {	
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-	
-    return 0;
+signed main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+    
+   return 0;
 }
